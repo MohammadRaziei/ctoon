a290b166563489a66615720329e7835ab9e30ea5
mohammadraziei <mohammadraziei1375@gmail.com>
2026-04-14 10:58:47 +0330

    feat(python): refactor bindings for improved type conversion and API clarity
    - Replace `unordered_map` and `variant` includes with `vector` and `map` for better compatibility
    - Add explicit `ctoon::` namespace qualifiers in `dict2value` function
    - Introduce comprehensive `value2dict` conversion with proper error handling
    - Rename `ToonOptions` to `EncodeOptions` to align with internal naming
    - Remove outdated header comments and streamline module initialization
    - Enhance Python-C++ type mappings with robust primitive and container support

--- src/bindings/python/binding.cpp
+++ src/bindings/python/binding.cpp
@@ -1,50 +1,45 @@
-//
-// Created by mohammad on 11/1/25.
-//
-
-// ctoon_bind.cpp
 #include <nanobind/nanobind.h>
 #include <nanobind/stl/string.h>
-#include <nanobind/stl/unordered_map.h>
-#include <nanobind/stl/variant.h>
+#include <nanobind/stl/vector.h>
+#include <nanobind/stl/map.h>
 #include "ctoon.h"
-
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
-
 namespace nb = nanobind;
 
-
+// Convert Python dict/list/primitive to ctoon::Value
 ctoon::Value dict2value(nb::handle obj) {
     if (obj.is_none()) {
-        return Value(nullptr);
+        return ctoon::Value(nullptr);
     } else if (nb::isinstance<nb::bool_>(obj)) {
-        return Value(static_cast<bool>(nb::cast<bool>(obj)));
+        return ctoon::Value(static_cast<bool>(nb::cast<bool>(obj)));
     } else if (nb::isinstance<nb::int_>(obj)) {
-        return Value(static_cast<int64_t>(nb::cast<int64_t>(obj)));
+        return ctoon::Value(static_cast<int64_t>(nb::cast<int64_t>(obj)));
     } else if (nb::isinstance<nb::float_>(obj)) {
-        return Value(static_cast<double>(nb::cast<double>(obj)));
+        return ctoon::Value(static_cast<double>(nb::cast<double>(obj)));
     } else if (nb::isinstance<nb::str>(obj)) {
-        return Value(nb::cast<std::string>(obj));
+        return ctoon::Value(nb::cast<std::string>(obj));
     } else if (nb::isinstance<nb::list>(obj)) {
         ctoon::Array arr;
         nb::list pylist = nb::cast<nb::list>(obj);
-        for (auto item : pylist)
+        for (auto item : pylist) {
             arr.push_back(dict2value(item));
-        return Value(arr);
+        }
+        return ctoon::Value(arr);
     } else if (nb::isinstance<nb::dict>(obj)) {
         ctoon::Object map;
         nb::dict d = nb::cast<nb::dict>(obj);
         for (auto [k, v] : d) {
             map[nb::cast<std::string>(k)] = dict2value(v);
         }
-        return Value(map);
+        return ctoon::Value(map);
     }
     throw std::runtime_error("Unsupported Python type for ctoon::Value conversion");
 }
 
+// Convert ctoon::Value to Python dict/list/primitive
 nb::object value2dict(const ctoon::Value& val) {
     if (val.isPrimitive()) {
         const auto& p = val.asPrimitive();
@@ -55,81 +50,129 @@ nb::object value2dict(const ctoon::Value& val) {
         }, p);
     } else if (val.isArray()) {
         nb::list out;
-        for (const auto& e : val.asArray())
+        for (const auto& e : val.asArray()) {
             out.append(value2dict(e));
+        }
         return std::move(out);
     } else if (val.isObject()) {
         nb::dict out;
-        for (const auto& kv : val.asObject())
+        for (const auto& kv : val.asObject()) {
             out[nb::cast(kv.first)] = value2dict(kv.second);
+        }
         return std::move(out);
     }
     throw std::runtime_error("Unknown ctoon::Value type");
 }
 
+NB_MODULE(ctoon_py, m) {
+    // Version information
+    m.attr("__version__") = ctoon::Version::string();
 
-template<typename Func, typename... Args>
-std::string dumpsDict(Func f, const ctoon::Object& dict, Args&&... args) {
-    ctoon::Value v(dict);
-    return f(v, std::forward<Args>(args)...);
-}
-
-template<typename Func, typename... Args>
-std::string dumpDict(Func f, const ctoon::Object& dict, Args&&... args) {
-    ctoon::Value v(dict);
-    return f(v, std::forward<Args>(args)...);
-
-// loadsXDict : str -> dict
-template<typename Func, typename... Args>
-ctoon::Object loadsDict(Func f, const std::string& s, Args&&... args) {
-    ctoon::Value v = f(s, std::forward<Args>(args)...);
-    return v.asObject();
-}
-
-
-
-NB_MODULE(NB_MODULE_NAME, m) {
-    nb::class_<ctoon::Value>(m, "Value")
-    .def(nb::init<>())
-    .def("is_object", &ctoon::Value::isObject)
-    .def("is_array", &ctoon::Value::isArray)
-    .def("is_primitive", &ctoon::Value::isPrimitive);
-
+    // Delimiter enum
     nb::enum_<ctoon::Delimiter>(m, "Delimiter")
         .value("Comma", ctoon::Delimiter::Comma)
         .value("Tab", ctoon::Delimiter::Tab)
         .value("Pipe", ctoon::Delimiter::Pipe);
 
-    nb::class_<ctoon::ToonOptions>(m, "ToonOptions")
+    // EncodeOptions class
+    nb::class_<ctoon::EncodeOptions>(m, "EncodeOptions")
         .def(nb::init<>())
         .def(nb::init<int>(), nb::arg("indent"))
-        .def("set_indent", &ctoon::ToonOptions::setIndent, nb::arg("indent"), nb::rv_policy::reference_internal)
-        .def("set_delimiter", &ctoon::ToonOptions::setDelimiter, nb::arg("delimiter"), nb::rv_policy::reference_internal)
-        .def("set_length_marker", &ctoon::ToonOptions::setLengthMarker, nb::arg("enabled"), nb::rv_policy::reference_internal)
-        .def("set_strict", &ctoon::ToonOptions::setStrict, nb::arg("strict"), nb::rv_policy::reference_internal)
-        .def("indent", &ctoon::ToonOptions::indent)
-        .def("delimiter", &ctoon::ToonOptions::delimiter)
-        .def("length_marker", &ctoon::ToonOptions::lengthMarker)
-        .def("strict", &ctoon::ToonOptions::strict);
+        .def_rw("indent", &ctoon::EncodeOptions::indent)
+        .def_rw("delimiter", &ctoon::EncodeOptions::delimiter)
+        .def_rw("length_marker", &ctoon::EncodeOptions::lengthMarker)
+        .def("set_indent", &ctoon::EncodeOptions::setIndent, nb::arg("indent"), nb::rv_policy::reference_internal)
+        .def("set_delimiter", &ctoon::EncodeOptions::setDelimiter, nb::arg("delimiter"), nb::rv_policy::reference_internal)
+        .def("set_length_marker", &ctoon::EncodeOptions::setLengthMarker, nb::arg("enabled"), nb::rv_policy::reference_internal);
 
-    m.def("value_loads_json", &ctoon::loadsJson);
-    m.def("value_dumps_json", &ctoon::dumpsJson);
-    m.def("value_loads_toon",
-          [](const std::string& toon, const ctoon::ToonOptions& options) {
-              return ctoon::loadsToon(toon, options);
-          },
-          nb::arg("toon"), nb::arg("options") = ctoon::ToonOptions());
-    m.def("value_dumps_toon",
-          [](const ctoon::Value& value, const ctoon::ToonOptions& options) {
-              return ctoon::dumpsToon(value, options);
-          },
-          nb::arg("value"), nb::arg("options") = ctoon::ToonOptions());
+    // DecodeOptions class
+    nb::class_<ctoon::DecodeOptions>(m, "DecodeOptions")
+        .def(nb::init<>())
+        .def(nb::init<bool>(), nb::arg("strict"))
+        .def_rw("strict", &ctoon::DecodeOptions::strict)
+        .def_rw("indent", &ctoon::DecodeOptions::indent)
+        .def("set_strict", &ctoon::DecodeOptions::setStrict, nb::arg("strict"), nb::rv_policy::reference_internal)
+        .def("set_indent", &ctoon::DecodeOptions::setIndent, nb::arg("indent"), nb::rv_policy::reference_internal);
 
-    m.def("loads_json", &ctoon::loadsJson)
+    // JSON functions
+    m.def("loads_json",
+        [](const std::string& json) {
+            return value2dict(ctoon::loadsJson(json));
+        },
+        nb::arg("json"));
 
-    #ifdef VERSION_INFO
-        m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
-    #else
-        m.attr("__version__") = "dev";
-    #endif
+    m.def("dumps_json",
+        [](nb::handle obj, int indent = 2) {
+            return ctoon::dumpsJson(dict2value(obj), indent);
+        },
+        nb::arg("data"), nb::arg("indent") = 2);
+
+    m.def("load_json",
+        [](const std::string& filename) {
+            return value2dict(ctoon::loadJson(filename));
+        },
+        nb::arg("filename"));
+
+    m.def("dump_json",
+        [](nb::handle obj, const std::string& filename, int indent = 2) {
+            ctoon::dumpJson(dict2value(obj), filename, indent);
+        },
+        nb::arg("data"), nb::arg("filename"), nb::arg("indent") = 2);
+
+    // TOON encode/decode (main API)
+    m.def("encode",
+        [](nb::handle obj, const ctoon::EncodeOptions& options = {}) {
+            return ctoon::encode(dict2value(obj), options);
+        },
+        nb::arg("data"), nb::arg("options") = ctoon::EncodeOptions());
+
+    m.def("decode",
+        [](const std::string& input, const ctoon::DecodeOptions& options = {}) {
+            return value2dict(ctoon::decode(input, options));
+        },
+        nb::arg("input"), nb::arg("options") = ctoon::DecodeOptions());
+
+    m.def("encode_to_file",
+        [](nb::handle obj, const std::string& filename, const ctoon::EncodeOptions& options = {}) {
+            ctoon::encodeToFile(dict2value(obj), filename, options);
+        },
+        nb::arg("data"), nb::arg("filename"), nb::arg("options") = ctoon::EncodeOptions());
+
+    m.def("decode_from_file",
+        [](const std::string& filename, const ctoon::DecodeOptions& options = {}) {
+            return value2dict(ctoon::decodeFromFile(filename, options));
+        },
+        nb::arg("filename"), nb::arg("options") = ctoon::DecodeOptions());
+
+    // Legacy TOON functions
+    m.def("loads_toon",
+        [](const std::string& toon, bool strict = true) {
+            return value2dict(ctoon::loadsToon(toon, strict));
+        },
+        nb::arg("toon"), nb::arg("strict") = true);
+
+    m.def("dumps_toon",
+        [](nb::handle obj, const ctoon::EncodeOptions& options = {}) {
+            return ctoon::dumpsToon(dict2value(obj), options);
+        },
+        nb::arg("data"), nb::arg("options") = ctoon::EncodeOptions());
+
+    // Generic format functions
+    m.def("loads",
+        [](const std::string& content, ctoon::Type format) {
+            return value2dict(ctoon::loads(content, format));
+        },
+        nb::arg("content"), nb::arg("format"));
+
+    m.def("dumps",
+        [](nb::handle obj, ctoon::Type format, int indent = 2) {
+            return ctoon::dumps(dict2value(obj), format, indent);
+        },
+        nb::arg("data"), nb::arg("format"), nb::arg("indent") = 2);
+
+    // Type enum
+    nb::enum_<ctoon::Type>(m, "Type")
+        .value("JSON", ctoon::Type::JSON)
+        .value("TOON", ctoon::Type::TOON)
+        .value("UNKNOWN", ctoon::Type::UNKNOWN);
 }
