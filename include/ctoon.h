115cb4e13e89f55a192723fc8671bf8bdde3ab5b
mohammadraziei <mohammadraziei1375@gmail.com>
2026-08-31 02:14:51 +0330

    Release 0.5.0

--- include/ctoon.h
+++ include/ctoon.h
@@ -487,7 +487,7 @@ extern "C" {
 #define CTOON_VERSION_MAJOR 0
 
 /** The minor version of ctoon. */
-#define CTOON_VERSION_MINOR 4
+#define CTOON_VERSION_MINOR 5
 
 /** The patch version of ctoon. */
 #define CTOON_VERSION_PATCH 0
